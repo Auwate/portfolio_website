046abf8aab4a66237bb994142a412cf3140e6c9f
Auwate <auwate1@gmail.com>
2024-11-26 19:24:28 -0500

    Commit:
    # Changes
    ---
    - Added design modeled for iPad Pro
    - Added design modeled for iPhone SE

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function toggleMenu() {
     const menu = document.querySelector(".mobile_nav_links")
+    const background = document.querySelector(".mobile_menu")
     const icon = document.querySelector(".mobile_hamburger_icon")
     const body = document.body
 
+    background.classList.toggle("cover-background")
     menu.classList.toggle("mobile_open")
     icon.classList.toggle("mobile_icon_open")
     body.classList.toggle("mobile_menu_open")
