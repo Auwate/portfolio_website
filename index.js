b89cb23061d74862784e7dd961845558635f33fc
Auwate <auwate1@gmail.com>
2024-11-29 09:05:01 -0500

    Commit:
    # Changes
    ---
    - Fixed a bug where the contact and footer section were not interactable, due to stack context issues

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function toggleMenu() {
     const background = document.querySelector(".mobile_menu")
     const icon = document.querySelector(".mobile_hamburger_icon")
     const body = document.body
+    const links = document.querySelector(".mobile_links")
 
     background.classList.toggle("cover-background")
     menu.classList.toggle("mobile_open")
     icon.classList.toggle("mobile_icon_open")
     body.classList.toggle("mobile_menu_open")
+    links.classList.toggle("mobile_open")
 }
